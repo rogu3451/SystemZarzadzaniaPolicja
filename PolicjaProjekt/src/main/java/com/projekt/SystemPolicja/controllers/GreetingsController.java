f98fe82985db64ebbed8b405661cb28f361a17e2
Karol <52329260+rogu3451@users.noreply.github.com>
2019-11-05 01:24:48 +0100

    Microsoft SQL - first query
    Pierwsze działające zapytanie do bazy, wypisanie wartości w pliku html

--- PolicjaProjekt/src/main/java/com/projekt/SystemPolicja/controllers/GreetingsController.java
+++ PolicjaProjekt/src/main/java/com/projekt/SystemPolicja/controllers/GreetingsController.java
@@ -3,22 +3,44 @@ package com.projekt.SystemPolicja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.sql.*;
 
 
 @Controller
 public class GreetingsController {
     @RequestMapping("/helloworld")
-    public String greeting(@RequestParam(value="name2", required=false, defaultValue="World") String nValue, Model model)
+    public String greeting(Model model)
     {
         Connection testConnection = getConnectionToDB();
-        System.out.println("Połączono z baza danych");
-        model.addAttribute("name",nValue);
+        String greeting = "";
+        if(testConnection == null)
+        {
+            greeting = "No Connection";
+        }
+        else
+        {
+            greeting = GetGreeting(testConnection);
+        }
+        model.addAttribute("Greeting", greeting);
         return "helloworld";
     }
 
-
+    public String GetGreeting(Connection conn)
+    {
+        Statement stmt = null;
+        String query = "SELECT TOP 1 imie FROM PROJEKT_SIEC_APTEK.dbo.[APTEKA.LEKARZ]";
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            return rs.getString("imie");
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+            return "";
+        }
+    }
     public Connection getConnectionToDB()
     {
         try
@@ -27,6 +49,7 @@ public class GreetingsController {
                     "jdbc:sqlserver://DESKTOP-P8ERKEM\\KAROLSQL",
                     "spring",
                     "123");
+
             return conn;
 
         }
