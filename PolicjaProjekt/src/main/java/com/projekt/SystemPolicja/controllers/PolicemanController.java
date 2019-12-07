09d19f79356c74a2bdde0a973d7e1a27d9fa5644
Karol <52329260+rogu3451@users.noreply.github.com>
2019-12-07 21:20:18 +0100

    Dodawanie Sprawy , Wystawianie mandatu
    Zrobione dodawanie sprawy i mandatu do bazy danych

--- PolicjaProjekt/src/main/java/com/projekt/SystemPolicja/controllers/PolicemanController.java
+++ PolicjaProjekt/src/main/java/com/projekt/SystemPolicja/controllers/PolicemanController.java
@@ -4,7 +4,10 @@ import com.projekt.SystemPolicja.Mandat;
 import com.projekt.SystemPolicja.Sprawa;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.sql.*;
 
 import java.util.ArrayList;
@@ -39,6 +42,93 @@ public class PolicemanController {
     }
 
 
+    @RequestMapping(value="/dodajSprawe", method= RequestMethod.GET)
+    public String getCaseForm()
+    {
+        // zwroc nazwe strony html
+        return "dodajSprawe";
+    }
+
+    @RequestMapping(value="/dodajSprawe", method=RequestMethod.POST)
+    public String dodajSprawe(@ModelAttribute(name="addCase") Sprawa sprawa, Model model)
+    {
+        Connection connection = getConnectionToDB();
+        int id = sprawa.getId();
+        String data = sprawa.getDate();
+        String status = sprawa.getStatus();
+        String opis = sprawa.getOpis();
+        String imie = sprawa.getImie();
+        String nazwisko = sprawa.getNazwisko();
+        String pesel = sprawa.getPesel();
+        addCase(connection, data, status, opis, imie, nazwisko, pesel);
+        model.addAttribute("dodanoSprawe",true);
+        return "dodajSprawe";
+    }
+
+    @RequestMapping(value="/dodajMandat", method= RequestMethod.GET)
+    public String getMandateForm()
+    {
+        // zwroc nazwe strony html
+        return "dodajMandat";
+    }
+
+    @RequestMapping(value="/dodajMandat", method=RequestMethod.POST)
+    public String dodajMandat(@ModelAttribute(name="addMandate") Mandat mandat, Model model)
+    {
+        Connection connection = getConnectionToDB();
+        int id = mandat.getId();
+        String data =  mandat.getDate();
+        String imie =  mandat.getImie();
+        String nazwisko =  mandat.getNazwisko();
+        String opis =  mandat.getOpis();
+        String kwota =  mandat.getKwota();
+        String pesel =  mandat.getPesel();
+        String adres =  mandat.getAdres();
+
+        addMandate(connection, data, imie, nazwisko, opis, kwota, pesel, adres);
+        model.addAttribute("dodanoMandat",true);
+        return "dodajMandat";
+    }
+
+    public void addMandate(Connection conn, String data, String imie, String nazwisko, String opis, String kwota, String pesel, String adres)
+    {
+        String query = "INSERT INTO POLICJA.dbo.MANDATE(data, imie, nazwisko, opis, kwota, pesel, adres) VALUES(?,?,?,?,?,?,?);";
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setString(1, data);
+            pst.setString(2, imie);
+            pst.setString(3, nazwisko);
+            pst.setString(4, opis);
+            pst.setString(5, kwota);
+            pst.setString(6, pesel);
+            pst.setString(7, adres);
+            pst.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+
+
+    public void addCase(Connection conn, String data, String status, String opis, String imie, String nazwisko, String pesel)
+    {
+        String query = "INSERT INTO POLICJA.dbo.SPRAWA(data, status, opis, imie, nazwisko, pesel) VALUES(?,?,?,?,?,?);";
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setString(1, data);
+            pst.setString(2, status);
+            pst.setString(3, opis);
+            pst.setString(4, imie);
+            pst.setString(5, nazwisko);
+            pst.setString(6, pesel);
+            pst.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
+    }
 
 
     public ArrayList<Mandat> GetAllMandates(Connection conn)
